a2f52c6a4cb8f5985d2c7c1f32c7d6db88a0ca65
Tey Taghiyev <54673844+sseagull@users.noreply.github.com>
2023-11-13 13:26:52 -0500

    app rename

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.scss'
-import App from './app-n'
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
