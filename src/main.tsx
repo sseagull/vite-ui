65fd593106c55051f89a23be8cd11e623f6d2db0
Tey Taghiyev <54673844+sseagull@users.noreply.github.com>
2023-11-13 13:26:22 -0500

    alias and test cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.scss'
-import App from './App.tsx'
+import App from './app-n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
